59529e09384aa26c64b708488f94c32bca01ded7
Bsir Rachida <roulgamous@gmail.com>
2021-12-02 22:13:53 +0100

    validation

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=ProgramRepository::class)
@@ -27,9 +28,7 @@ class Program
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank()
-
      * @Assert\Length(max="255")
-
      */
     private $title;
 
@@ -38,7 +37,7 @@ class Program
      */
     private $summary;
 
-    /**
+       /**
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank()
      * @Assert\Regex(
